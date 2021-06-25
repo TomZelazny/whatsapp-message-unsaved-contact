ac47e52f20256d0c28fa9095b60c3ebe71816432
TomZelazny <tom.zelazny@mail.huji.ac.il>
2021-06-25 03:35:21 +0300

    moved everything to vendors

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#phone");
 const statusElement = document.querySelector("#status");
 
 var iti = window.intlTelInput(input, {
-    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.13/js/utils.min.js",
+    utilsScript: "/vendors/utils.min.js",
 });
 iti.promise.then(function () {
     statusElement.innerHTML = "Initialised!";
